e86d3792fcd0b7616f1cf6bdee604bf13e868053
hash <hashesandsalts000@gmail.com>
2024-04-27 16:33:56 +0545

    add 404 not found page

---
+++ frontend/src/components/PageNotFound.tsx
@@ -0,0 +1,7 @@
+const PageNotFound = () => {
+  return (
+    <div>error Page Not Found</div>
+  )
+}
+
+export default PageNotFound
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import PageNotFound from './components/PageNotFound.tsx'
+import Register from './pages/Register.tsx'
+import Login from './pages/Login.tsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+    errorElement: <PageNotFound/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+    errorElement: <PageNotFound/>
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+    errorElement: <PageNotFound/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
